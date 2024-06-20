797ffee71d03a09d1a72ea1224c126de3fdf754e
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-06-20 17:11:24 -0300

    Adicionado o banco de dados h2 e criação automatica da base de dados - seção6-aula49

--- Seção6-APIJava+SpringBoot+JPA/cursomc/src/main/java/com/bruno/cursomc/domain/Categoria.java
+++ Seção6-APIJava+SpringBoot+JPA/cursomc/src/main/java/com/bruno/cursomc/domain/Categoria.java
@@ -3,13 +3,22 @@ package com.bruno.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	//Consturtor vazio
+	//Construtor vazio
 	public Categoria() {
 	}
 
@@ -56,6 +65,4 @@ public class Categoria implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
 }
