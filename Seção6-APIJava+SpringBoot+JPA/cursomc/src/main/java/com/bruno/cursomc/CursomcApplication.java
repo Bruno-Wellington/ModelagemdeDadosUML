89d703afe1b3977b4c6d126d24d757a6f1ece2b9
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-06-21 15:02:44 -0300

    Criando operação de instanciação - seção6-aula51

--- Seção6-APIJava+SpringBoot+JPA/cursomc/src/main/java/com/bruno/cursomc/CursomcApplication.java
+++ Seção6-APIJava+SpringBoot+JPA/cursomc/src/main/java/com/bruno/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.bruno.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.bruno.cursomc.domain.Categoria;
+import com.bruno.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired //faz a injeção de dependendia automaticamente
+	private CategoriaRepository categoriaRepository;//Dependencia
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
 
 }
