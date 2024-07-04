ef9511246bee86b944c7aeeb403f7839e18ab6d0
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-07-04 17:38:39 -0300

    Seção4-aula3 - v3

---
+++ Seção4/Exerc03-APIJava+SpringBoot+JPA/atividade3secao4/src/main/java/com/bruno/atividade2secao4/resources/exceptions/ResourceExceptionHandller.java
@@ -0,0 +1,21 @@
+package com.bruno.atividade2secao4.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bruno.atividade2secao4.services.exceptions.ObjNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandller {
+
+	@ExceptionHandler(ObjNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
--- Seção4/Exerc03-APIJava+SpringBoot+JPA/atividade3secao4/src/main/java/com/bruno/atividade2secao4/services/UsuarioService.java
+++ Seção4/Exerc03-APIJava+SpringBoot+JPA/atividade3secao4/src/main/java/com/bruno/atividade2secao4/services/UsuarioService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.bruno.atividade2secao4.domain.Usuario;
 import com.bruno.atividade2secao4.repositories.UsuarioRepository;
+import com.bruno.atividade2secao4.services.exceptions.ObjNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -17,8 +18,8 @@ public class UsuarioService {
 	
 	public Usuario find(Integer id) {
 		Optional<Usuario> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto nao encontrado para o id: "+ id + ", Tipo" + Usuario.class.getName()));
+		return obj.orElseThrow(() -> new ObjNotFoundException(
+				"Objeto nao encontrado para o id: " + id + ", Tipo: " + Usuario.class.getName()));
 	}
 
 }
